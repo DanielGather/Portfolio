d1255018a80767f4bbd730380f397c79dbc60c3b
Loew3nzahn <gather.daniel@yahoo.de>
2025-02-20 14:21:04 +0100

    responsive above the fold

--- src/app/main-page/above-the-fold/above-the-fold.component.ts
+++ src/app/main-page/above-the-fold/above-the-fold.component.ts
@@ -11,19 +11,24 @@ import { BannerComponent } from './banner/banner.component';
 export class AboveTheFoldComponent {
   githubImage: string = './../../../assets/img/greenGithub.webp';
   linkedInImage: string = './../../../assets/img/greenLinkedIn.webp';
+  mailImage: string = './../../../assets/img/mail.webp';
 
   changeImage(image: string) {
     if (image == 'github') {
       this.githubImage = './../../../assets/img/hoverGithub.webp';
-    } else {
+    } else if (image == 'linkedIn') {
       this.linkedInImage = './../../../assets/img/hoverLinkedIn.webp';
+    } else {
+      this.mailImage = './../../../assets/img/hoverMail.webp';
     }
   }
   resetImage(image: string) {
     if (image == 'github') {
       this.githubImage = './../../../assets/img/greenGithub.webp';
-    } else {
+    } else if (image == 'linkedIn') {
       this.linkedInImage = './../../../assets/img/greenLinkedIn.webp';
+    } else {
+      this.mailImage = './../../../assets/img/mail.webp';
     }
   }
 }
