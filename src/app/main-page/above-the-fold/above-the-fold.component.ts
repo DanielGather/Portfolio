f3d3b7c55807b871f69c202374525912de89678d
Loew3nzahn <gather.daniel@yahoo.de>
2025-02-20 19:05:34 +0100

    resize

--- src/app/main-page/above-the-fold/above-the-fold.component.ts
+++ src/app/main-page/above-the-fold/above-the-fold.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs';
 import { BannerComponent } from './banner/banner.component';
 
 @Component({
@@ -8,10 +10,30 @@ import { BannerComponent } from './banner/banner.component';
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss',
 })
-export class AboveTheFoldComponent {
+export class AboveTheFoldComponent implements OnInit, OnDestroy {
+  constructor() {}
+
+  resizeSubscription?: Subscription;
+
   githubImage: string = './../../../assets/img/greenGithub.webp';
   linkedInImage: string = './../../../assets/img/greenLinkedIn.webp';
   mailImage: string = './../../../assets/img/mail.webp';
+
+  responsive?: boolean;
+
+  ngOnInit(): void {
+    this.resizeSubscription = fromEvent(window, 'resize')
+      .pipe(debounceTime(200)) // Verhindert, dass zu viele Events hintereinander verarbeitet werden
+      .subscribe((event: Event) => {
+        console.log('Resize-Event erkannt:', event);
+        if (window.innerWidth < 700) {
+          this.responsive = true;
+        } else if (window.innerWidth > 700) {
+          this.responsive = false;
+        }
+        // Hier kannst du weitere Logik einfügen
+      });
+  }
 
   changeImage(image: string) {
     if (image == 'github') {
@@ -29,6 +51,14 @@ export class AboveTheFoldComponent {
       this.linkedInImage = './../../../assets/img/greenLinkedIn.webp';
     } else {
       this.mailImage = './../../../assets/img/mail.webp';
+    }
+  }
+
+  checkResize() {}
+
+  ngOnDestroy(): void {
+    if (this.resizeSubscription) {
+      this.resizeSubscription.unsubscribe();
     }
   }
 }
