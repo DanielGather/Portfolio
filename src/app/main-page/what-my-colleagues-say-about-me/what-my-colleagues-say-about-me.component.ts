c68404c882c126c4e1d24186b4e258fc22951c96
Loew3nzahn <gather.daniel@yahoo.de>
2025-03-02 21:03:44 +0100

    started what my collagues say about me

--- src/app/main-page/what-my-colleagues-say-about-me/what-my-colleagues-say-about-me.component.ts
+++ src/app/main-page/what-my-colleagues-say-about-me/what-my-colleagues-say-about-me.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-what-my-colleagues-say-about-me',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './what-my-colleagues-say-about-me.component.html',
-  styleUrl: './what-my-colleagues-say-about-me.component.scss'
+  styleUrl: './what-my-colleagues-say-about-me.component.scss',
 })
 export class WhatMyColleaguesSayAboutMeComponent {
+  reviews = [
+    {
+      text: 'Daniel has proven to be a reliable group partner. His technical skills and proactive approach were crucial to the succcess of our project.',
+      name: 'Rodger Bongartz',
+      role: 'Team Partner',
+    },
+    {
+      text: 'Daniel has proven to be a reliable group partner. His technical skills and proactive approach were crucial to the succcess of our project.',
+      name: 'Peter Pansen',
+      role: 'Senior Frontend Developer',
+    },
+    {
+      text: 'Daniel has proven to be a reliable group partner. His technical skills and proactive approach were crucial to the succcess of our project.',
+      name: 'Babsi',
+      role: 'Head of All',
+    },
+  ];
 
+  focus: boolean = true;
 }
