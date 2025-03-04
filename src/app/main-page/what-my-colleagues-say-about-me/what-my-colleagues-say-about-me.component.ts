42bcc0132a4b7eab2fa7758574d9157d9c74cc75
Loew3nzahn <gather.daniel@yahoo.de>
2025-03-04 12:27:16 +0100

    pointer with renderer

--- src/app/main-page/what-my-colleagues-say-about-me/what-my-colleagues-say-about-me.component.ts
+++ src/app/main-page/what-my-colleagues-say-about-me/what-my-colleagues-say-about-me.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { NgStyle } from '@angular/common';
+import { Renderer2 } from '@angular/core';
+import { ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-what-my-colleagues-say-about-me',
@@ -10,6 +12,11 @@ import { NgStyle } from '@angular/common';
   styleUrl: './what-my-colleagues-say-about-me.component.scss',
 })
 export class WhatMyColleaguesSayAboutMeComponent {
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+  @ViewChild('pointLeft') pointLeft!: ElementRef;
+  @ViewChild('pointMiddle') pointMiddle!: ElementRef;
+  @ViewChild('pointRight') pointRight!: ElementRef;
+
   activeIndex: number = 1;
   leftButtonIsClicked: boolean = false;
   rightButtonIsClicked: boolean = false;
@@ -53,9 +60,11 @@ export class WhatMyColleaguesSayAboutMeComponent {
     if (this.leftButtonIsClicked) {
       this.leftButtonIsClicked = false;
       this.resetPosition = true;
+      this.handlePointerColor('pointMiddle');
     } else if (!this.leftButtonIsClicked && this.resetPosition) {
       this.resetPosition = false;
       this.rightButtonIsClicked = true;
+      this.handlePointerColor('pointRight');
     }
     this.rightButtonIsClicked = true;
     if (this.activeIndex < this.reviews.length - 1) {
@@ -63,23 +72,50 @@ export class WhatMyColleaguesSayAboutMeComponent {
     }
   }
 
-  pointerLeft() {
+  pointerLeft(event: Event, point: string) {
+    this.handlePointerColor(point);
     this.rightButtonIsClicked = false;
     this.resetPosition = false;
     this.activeIndex--;
     this.leftButtonIsClicked = true;
   }
 
-  pointerMiddle() {
+  pointerMiddle(event: Event, point: string) {
+    this.handlePointerColor(point);
     this.rightButtonIsClicked = false;
     this.leftButtonIsClicked = false;
     this.resetPosition = true;
     this.activeIndex = 1;
   }
-  pointerRight() {
+  /*************  ✨ Codeium Command 🌟  *************/
+  pointerRight(event: Event, point: string) {
+    this.handlePointerColor(point);
     this.leftButtonIsClicked = false;
     this.resetPosition = false;
     this.activeIndex++;
     this.rightButtonIsClicked = true;
+  }
+  /******  145370a1-d6ae-4ef9-a3f2-634c98ebf436  *******/
+  handlePointerColor(point: string) {
+    this.removePointFillColor();
+    let currentPoint = this.changeColor(point);
+    this.renderer.setStyle(currentPoint?.nativeElement, 'fill', '#3dcfb6');
+  }
+
+  changeColor(point: string) {
+    return point === 'pointLeft'
+      ? this.pointLeft
+      : point === 'pointRight'
+      ? this.pointRight
+      : point === 'pointMiddle'
+      ? this.pointMiddle
+      : null;
+  }
+
+  removePointFillColor() {
+    const allCircle = this.el.nativeElement.querySelectorAll('circle');
+    allCircle.forEach((circle: HTMLElement) =>
+      this.renderer.setStyle(circle, 'fill', 'white')
+    );
   }
 }
