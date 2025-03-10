a04fab4105437a81e43d619a19a3dc4523057ed9
Daniel Gather <163533297+DanielGather@users.noreply.github.com>
2025-03-10 16:20:36 +0100

    BurgerMenu Implemented

--- src/app/main-page/shared/header/header.component.ts
+++ src/app/main-page/shared/header/header.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { SwitchLanguageComponent } from '../../switch-language/switch-language.component';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { DOCUMENT } from '@angular/common';
+import { Inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 @Component({
   selector: 'app-header',
   imports: [SwitchLanguageComponent, TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router,
+    private scroller: ViewportScroller
+  ) {}
+  burgerMenuOpen: boolean = false;
+
+  toggleBurgerMenu(event: Event) {
+    setTimeout(() => {
+      event.preventDefault();
+      event?.stopPropagation();
+      if (this.burgerMenuOpen) {
+        this.renderer.removeStyle(this.document.body, 'overflow');
+      } else {
+        this.renderer.setStyle(this.document.body, 'overflow', 'hidden');
+      }
+      this.burgerMenuOpen = !this.burgerMenuOpen;
+    }, 10);
+  }
+
+  scrollToAnchor() {
+    this.scroller.scrollToAnchor('skills');
+  }
+
+  closeBurgerMenu(event: Event) {
+    event.stopPropagation();
+  }
+}
