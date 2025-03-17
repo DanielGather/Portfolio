f506489bcaa8d6708040cff78542d48633086e31
Loew3nzahn <gather.daniel@yahoo.de>
2025-03-17 22:31:19 +0100

    translate privacy policy

--- src/app/main-page/privacy-policy/privacy-policy.component.ts
+++ src/app/main-page/privacy-policy/privacy-policy.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { SharedService } from '../../services/shared.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, TranslateModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss',
   host: {
